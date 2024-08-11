b7daec35c3b7fc41e569188832eba98a35d2f1a0
PR7175Z <aadimpraz@gmail.com>
2024-08-11 06:47:07 +0545

    Prevented parent node click on child node click

--- script.js
+++ script.js
@@ -8,10 +8,23 @@ document.addEventListener('DOMContentLoaded', ()=>{
         fchild.appendChild(p);
     }
 
+    const initialstate = fchild;
+
+    console.log(initialstate);
+
+    const lchild = root.children[2];
+    lchild.addEventListener('change', ()=>{
+        console.log('check');
+    });
+    // console.log(lchild);
+
     let diskval;
     const disks = document.querySelectorAll('.tower p');
     for(let i = 0; i<disks.length; i++){
-        disks[i].addEventListener('click', ()=>{
+        disks[i].addEventListener('click', (event)=>{
+            //prevent parent click trigger on child click trigger
+            event.stopPropagation();
+
             // remove all the data-state attribute on click
             disks.forEach((disk) => {
                 disk.removeAttribute('data-state');
@@ -27,8 +40,17 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     for(let i = 0; i<tower.length; i++){
         tower[i].addEventListener('click', () => {
+            // if(!tower[i].hasChildNodes()){
+            //     return;
+            // }
             let p = document.querySelector('[data-state="clicked"]');
-            tower[i].appendChild(p);
+            let firstchild = tower[i].querySelector('p');
+            if(p){
+                if(firstchild == null || p.innerHTML < firstchild.innerHTML){
+                    tower[i].prepend(p);
+                    p.removeAttribute('data-state');
+                }
+            }
         });
     }
 
