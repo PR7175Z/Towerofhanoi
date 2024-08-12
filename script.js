09d78973016341e4bcc264c28ca0dad4a7c69931
PR7175Z <aadimpraz@gmail.com>
2024-08-12 23:11:49 +0545

    Run everything after form submission

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const root = document.getElementById('root');
 
-    const fchild = root.children[0];
-    for(let j = 0; j<3;j++){
-        let p = document.createElement('p');
-        p.innerHTML = j;
-        fchild.appendChild(p);
-    }
-
     const formwrap = document.querySelector('.form-wrapper');
 
+    let size;
+    const fchild = root.children[0];
     document.getElementById('sizeForm').addEventListener('submit', (e)=>{
         e.preventDefault();
         formwrap.style.display = 'none';
         root.style.display = 'block';
+        size = document.querySelector('input[name="diskcount"]').value;
+
+        for(let j = 0; j<size;j++){
+            let p = document.createElement('p');
+            p.innerHTML = j;
+            p.style.background = 'red';
+            p.style.width = (j*4+20)+'px';
+            fchild.appendChild(p);
+        }
+
+        let diskval;
+        const disks = document.querySelectorAll('.tower p');
+        for(let i = 0; i<disks.length; i++){
+            disks[i].addEventListener('click', (event)=>{
+                //prevent parent click trigger on child click trigger
+                event.stopPropagation();
+
+                // remove all the data-state attribute on click
+                disks.forEach((disk) => {
+                    disk.removeAttribute('data-state');
+                });
+
+                diskval = disks[i].innerHTML;
+
+                //check if current item is at the top
+                if(disks[i].parentNode.children[0] === disks[i]){
+                    disks[i].setAttribute('data-state', 'clicked');
+                }
+                // console.log(disks[i].innerHTML);
+            });
+        }
+
+
+        const firsttower = root.children[0].innerHTML;
+        const lchild = root.children[2];
+        const tower = document.querySelectorAll('#root .tower');
+
+        for(let i = 0; i<tower.length; i++){
+            tower[i].addEventListener('click', () => {
+                let p = document.querySelector('[data-state="clicked"]');
+                let firstchild = tower[i].querySelector('p');
+                if(p){
+                    if(firstchild == null || p.innerHTML < firstchild.innerHTML){
+                        tower[i].prepend(p);
+                        p.removeAttribute('data-state');
+                    }
+                }
+                
+                if(firsttower ==  lchild.innerHTML){
+                    document.getElementById('result').innerHTML = 'You Win';
+                }
+
+            });
+        }
     })
 
     // const initialstate = fchild;
@@ -28,51 +77,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
     // });
     // console.log(lchild);
 
-    let diskval;
-    const disks = document.querySelectorAll('.tower p');
-    for(let i = 0; i<disks.length; i++){
-        disks[i].addEventListener('click', (event)=>{
-            //prevent parent click trigger on child click trigger
-            event.stopPropagation();
-
-            // remove all the data-state attribute on click
-            disks.forEach((disk) => {
-                disk.removeAttribute('data-state');
-            });
-
-            diskval = disks[i].innerHTML;
-
-            //check if current item is at the top
-            if(disks[i].parentNode.children[0] === disks[i]){
-                disks[i].setAttribute('data-state', 'clicked');
-            }
-            // console.log(disks[i].innerHTML);
-        });
-    }
-
-
-    const firsttower = root.children[0].innerHTML;
-    const lchild = root.children[2];
-    const tower = document.querySelectorAll('#root .tower');
-
-    for(let i = 0; i<tower.length; i++){
-        tower[i].addEventListener('click', () => {
-            let p = document.querySelector('[data-state="clicked"]');
-            let firstchild = tower[i].querySelector('p');
-            if(p){
-                if(firstchild == null || p.innerHTML < firstchild.innerHTML){
-                    tower[i].prepend(p);
-                    p.removeAttribute('data-state');
-                }
-            }
-            
-            if(firsttower ==  lchild.innerHTML){
-                document.getElementById('result').innerHTML = 'You Win'
-                console.log('win')
-            }
-
-        });
-    }
 
     // disks.addEventListener('click', ()=>{
     //     console.log(this.innerHTML);
