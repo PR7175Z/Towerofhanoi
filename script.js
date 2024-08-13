4da3ea3fa990cac7f8417cd1af4c3518b018ee85
PR7175Z <aadimpraz@gmail.com>
2024-08-13 13:41:40 +0545

    Added losing condition and move counter

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const root = document.getElementById('root');
 
     const formwrap = document.querySelector('.form-wrapper');
-
+    const movecounter = document.querySelector('.movecounter');
+    const movecountdown = document.getElementById('movecount');
     let size;
     const fchild = root.children[0];
     document.getElementById('sizeForm').addEventListener('submit', (e)=>{
         e.preventDefault();
         formwrap.style.display = 'none';
         root.style.display = 'block';
+        movecounter.style.display = 'block';
         size = document.querySelector('input[name="diskcount"]').value;
+
+        let movetheshold = (2**size) -1;
+        movecountdown.innerHTML = movetheshold;
 
         for(let j = 0; j<size;j++){
             let p = document.createElement('p');
             p.setAttribute('data-value', j);
-            // p.innerHTML = j;
             p.style.background = 'red';
-            p.style.width = (j*4+20)+'px';
-            p.style.minHeight = '20px';
+            p.style.width = (j*10+20)+'px';
+            p.style.minHeight = '15px';
             fchild.appendChild(p);
         }
 
@@ -39,10 +43,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 if(disks[i].parentNode.children[0] === disks[i]){
                     disks[i].setAttribute('data-state', 'clicked');
                 }
-                // console.log(disks[i].innerHTML);
             });
         }
-
 
         const firsttower = root.children[0].innerHTML;
         const lchild = root.children[2];
@@ -56,11 +58,18 @@ document.addEventListener('DOMContentLoaded', ()=>{
                     if(firstchild == null || p.getAttribute('data-value') < firstchild.getAttribute('data-value')){
                         tower[i].prepend(p);
                         p.removeAttribute('data-state');
+                        movetheshold--;
+                        movecountdown.innerHTML = movetheshold;
+                        if(movetheshold < 0){
+                            document.getElementById('status-message').innerHTML = 'You Loose';
+                        }
                     }
                 }
                 
                 if(firsttower ==  lchild.innerHTML){
                     document.getElementById('status-message').innerHTML = 'You Win';
+                    root.style.display = 'none';
+                    movecounter.style.display = 'none';
                     document.getElementById('result').style.display = 'block';
                 }
 
@@ -71,36 +80,4 @@ document.addEventListener('DOMContentLoaded', ()=>{
     document.querySelector('.cross').addEventListener('click', () => {
         window.location.reload();
     })
-    // const initialstate = fchild;
-
-    // function wincheck(initialstate = fchild){
-    //     console.log(initialstate)
-    // }
-
-    // const lchild = root.children[2];
-    // lchild.addEventListener('change', ()=>{
-    //     console.log('check');
-    // });
-    // console.log(lchild);
-
-
-    // disks.addEventListener('click', ()=>{
-    //     console.log(this.innerHTML);
-    // })
-
-    // console.log(root.children[0]);
-    // Array.from(root.children).forEach((element)=>{
-    //     console.log(element);
-    // })
-
-
-    // for(let i = 0; i<3; i++){
-    //     for(let j = 0; j<3;j++){
-    //         let p = document.createElement('p');
-    //         p.innerHTML = 'Hello World';
-    //         document.getElementById('root').appendChild(p);
-    //         console.log(a)
-    //     }
-    //     a++;
-    // }
 });
